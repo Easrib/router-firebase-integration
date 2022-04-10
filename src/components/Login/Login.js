b74f72869969ff8ab958c82f8564c15bb2a5415d
Easrib <easrib.sharik01@gmail.com>
2022-04-10 22:30:13 +0600

    requireAuth set up

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,29 @@
+import { getAuth } from 'firebase/auth';
 import React from 'react';
-import useFirebase from '../../hooks/useFirebase';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth'
+import { useLocation, useNavigate } from 'react-router-dom';
+import app from '../../firebase.init';
 
+const auth = getAuth(app)
 const Login = () => {
-    const { signInWithGoogle } = useFirebase();
+    const [signInWithGoogle, user] = useSignInWithGoogle(auth);
+    let navigate = useNavigate();
+    let location = useLocation();
+    let from = location.state?.from?.pathname || "/";
+
+
+    const handleGoogleSignIn = () => {
+        signInWithGoogle()
+            .then(() => {
+                navigate(from, { replace: true }
+            )})
+
+    }
+
     return (
         <div>
             <h2>Please Login</h2>
-            <button style={{ margin: "10px" }} onClick={signInWithGoogle}>Sign in With Google</button>
+            <button style={{ margin: "10px" }} onClick={handleGoogleSignIn}>Sign in With Google</button>
 
             <form action="">
                 <input type="email" name="email" id="" placeholder='Please write your email' /><br />
